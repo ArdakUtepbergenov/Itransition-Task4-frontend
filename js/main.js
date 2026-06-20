c48b78e6231da320f36d270ee1198bc8984332f4
Ardak <98506975+ArdakUtepbergenov@users.noreply.github.com>
2026-06-20 17:30:29 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,44 +1,41 @@
 function login(event){
     event.preventDefault();
-
     username = document.getElementById("username").value;
     password = document.getElementById("password").value;
 
     fetch("https://itransition-task4-production.up.railway.app/api/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username: username, password: password })
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: username, password: password })
     })
     .then(response => response.json())
     .then(data => {
-    if (data.success) {
-        localStorage.setItem("userId", data.userId);
-        window.location.href = `users.html?userId=${data.userId}`;
-    } else {
-        alert("Неверный логин или пароль");
-    }
-})
+        if (data.success) {
+            localStorage.setItem("sessionToken", data.sessionToken);
+            window.location.href = `users.html?sessionToken=${data.sessionToken}`;
+        } else {
+            alert("Неверный логин или пароль");
+        }
+    })
 }
 
 function register(event) {
     event.preventDefault();
-
     username = document.getElementById("username").value;
     password = document.getElementById("password").value;
     confirmPassword = document.getElementById("confirmPassword").value;
-    email  = document.getElementById("email").value;
+    email = document.getElementById("email").value;
 
     fetch("https://itransition-task4-production.up.railway.app/api/register", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username: username, password: password, confirmPassword: confirmPassword, email: email})
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: username, password: password, confirmPassword: confirmPassword, email: email})
     })
     .then(response => response.json())
     .then(data => {
         if(data.success){
-    localStorage.setItem("userId", data.userId);
-    window.location.href = `users.html?userId=${data.userId}`;
-
+            localStorage.setItem("sessionToken", data.sessionToken);
+            window.location.href = `users.html?sessionToken=${data.sessionToken}`;
         } else {
             alert(data.message);
         }
